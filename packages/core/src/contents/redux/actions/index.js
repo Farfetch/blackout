0646624cd93216d6a01ab59f06a47e4c9bb0a19e
João Ramalho Costa <joao.ramalhocosta@farfetch.com>
2021-11-30 16:28:05 +0000

    feat(core): add commerce pages redux
    . Add action doGetCommercePages
    . Update Content reducer
    . Add ranking logic to contents utils

--- packages/core/src/contents/redux/actions/index.js
+++ packages/core/src/contents/redux/actions/index.js
@@ -1,3 +1,12 @@
+/**
+ * Contents actions.
+ *
+ * @module contents/actions
+ * @category Contents
+ * @subcategory Actions
+ */
+
+export { default as doGetCommercePages } from './doGetCommercePages';
 export { default as doGetContent } from './doGetContent';
 export { default as doGetContentTypes } from './doGetContentTypes';
 export { default as doGetSEO } from './doGetSEO';
--- tests/__fixtures__/contents/index.js
+++ tests/__fixtures__/contents/index.js
@@ -1,3 +1,4 @@
+export * from './commercePages.fixtures';
 export * from './contentTypes.fixtures';
 export * from './contents.fixtures';
 export * from './seo.fixtures';
