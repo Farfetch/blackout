6ec89aa51d1e6935d2d9d0015652917d52e4a688
João Ramalho Costa <joao.ramalhocosta@farfetch.com>
2021-12-13 15:09:51 +0000

    chore: refactor yalc scripts
    Add just two scripts to publish and push, instead of having multiple scripts for each package.

---
+++ scripts/yalc-publish.sh
@@ -0,0 +1,19 @@
+package='yalc'
+if [ `npm list -g | grep -c $package` -eq 0 ]; then
+    npm install -g $package --no-shrinkwrap
+fi
+
+if [ ! $1 ]; 
+then
+  printf "If you want to publish a specific package, use the folder name as argument. \nExample: 'yarn yalc-publish core'.\n"
+  for i in ./packages/*
+  do
+    cd "$i"
+    yalc publish
+    cd ../..
+  done
+else
+  echo "Folder package selected: $1";
+  cd packages/$1
+  yalc publish
+fi
---
+++ scripts/yalc-push.sh
@@ -0,0 +1,9 @@
+if [ ! $1 ]; 
+then
+  printf "Missing package folder name. \nIf you want to push a specific package, use the folder name as argument. \nExample: 'yarn yalc-push core'. \n"
+  printf "\x1b[31mNo packages have been pushed.\x1b[0m\n";
+else
+  cd packages/$1
+  yalc push
+  cd ../..
+fi
