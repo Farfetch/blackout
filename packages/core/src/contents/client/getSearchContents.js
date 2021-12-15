4cfe6c10ac6d21086a6b8d31b49a1da8bed154fe
João Ramalho Costa <joao.ramalhocosta@farfetch.com>
2021-12-15 14:59:08 +0000

    docs(core): add new query parameter contents
    More query parameters listed on search contents endpoint documentation
    - target
    - metadataCustom
    - metadataSearchTagsValues

--- packages/core/src/contents/client/getSearchContents.js
+++ packages/core/src/contents/client/getSearchContents.js
@@ -2,6 +2,18 @@ import client, { adaptError } from '../../helpers/client';
 import join from 'proper-url-join';
 
 /**
+ * @typedef {object} QueryTarget
+ *
+ * @alias QueryTarget
+ * @memberof module:contents/client
+ *
+ * @property {string} [contentzone] - The zone to query by (ROW|EUROPE...).
+ * @property {string} [language] - Using country code to query by a specific language (US).
+ * @property {string} [country] - Using culture code to query by a specific country (en-US).
+ * @property {string} [benefits] - Query content for a benefit (sale).
+ */
+
+/**
  * Method responsible for searching the content that corresponds to the query object received.
  *
  * @function getSearchContents
@@ -11,10 +23,15 @@ import join from 'proper-url-join';
  * @param {string} query.spaceCode - The space the content belongs to (website|mobileapp|emailTool...).
  * @param {string} query.environmentCode - The environment identifier (live | preview).
  * @param {string} query.contentTypeCode - The content type unique code (page|post|menu|pages|posts|widgets|waterproof...).
- * @param {string | string[]} query.codes - List of codes that representing the content code (about-us|today-news|header|productId...).
- * @param {number} query.page - Number of the page to get, starting at 1. The default is 1.
- * @param {number} query.pageSize - Size of each page, as a number between 1 and 180. The default is 60.
+ * @param {string|string[]} [query.codes] - List of codes that representing the content code (about-us|today-news|header|productId...).
+ * @param {QueryTarget} [query.target] - The targets and respective values that a content type is configured `(contentzone:ROW | country:GB |
+ * language:en-GB | benefits:test)`. If no targets requested, the zone associated with the country specified in the FF-Country header will be used.
+ * @param {number} [query.page=1] - Number of the page to get, starting at 1. The default is 1.
+ * @param {number} [query.pageSize=60] - Size of each page, as a number between 1 and 180. The default is 60.
  * @param {string} [query.sort] - Sort content by (publicationDate:desc | publicationDate:asc | metadataCustom.eventDate:desc | metadataCustom.X:asc).
+ * @param {string} [query.metadataSearchTagsValues] - Filter by metadata tag. Separate multiple values with commas (,). For example, "red,dress".
+ * @param {string} [query.metadataCustom] - Filter by content custom metadata. Repeat the parameter for multiple values. For example,
+ * (metadataCustom.eventDate=2020-07-21&metadataCustom.eventName=cosmos&metadataCustom.presenter=carl sagan,tyson neil degrasse).
  * @param {object} [config] - Custom configurations to send to the client
  * instance (axios).
  *
