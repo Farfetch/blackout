9a69cc188005a5ebf34cd3b041604e7b027946eb
Nelson Leite <nelson.leite@farfetch.com>
2021-12-16 10:46:32 +0000

    fix(redux): remove circular dependency from recently viewed

--- packages/redux/src/recentlyViewed/actions/factories/fetchRecentlyViewedProductsFactory.js
+++ packages/redux/src/recentlyViewed/actions/factories/fetchRecentlyViewedProductsFactory.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../../actionTypes';
-import { areRecentlyViewedProductsFetched } from '../../';
+import { areRecentlyViewedProductsFetched } from '../../selectors';
 
 /* eslint-disable jsdoc/no-undefined-types */
 
