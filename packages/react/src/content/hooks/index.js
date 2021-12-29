fe333d21c72b09823170745303e2463de17b1df1
Ricardo Jorge Mota <ricardosilva.mota@farfetch.com>
2021-12-29 15:29:00 +0000

    feat(react): create hook commerce pages
    Create new hook to commerce page
    Fix redux doGetCommercePage to ranking work correctly
    
    JIRA Issue: FPSCONTENT-1223

--- packages/react/src/content/hooks/index.js
+++ packages/react/src/content/hooks/index.js
@@ -6,6 +6,7 @@
  * @subcategory hooks
  */
 
+export { default as useCommercePage } from './useCommercePage';
 export { default as useContentType } from './useContentType';
 export { default as useNavbars } from './useNavbars';
 export { default as usePage } from './usePage';
