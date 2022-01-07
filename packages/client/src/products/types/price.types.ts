f0cd63bf8ee0a7661c7f38278b39c82dc363db96
Nelson Leite <nelson.leite@farfetch.com>
2022-01-07 10:24:38 +0000

    chore(client): enhance product-related types and price adapter
    This introduces several enhancements to product-related types:
    - add some missing types
    - sort properties for better readability
    
    This also removes the `deep-compact` dependency from the price adapter, which no longer makes sense

--- packages/client/src/products/types/price.types.ts
+++ packages/client/src/products/types/price.types.ts
@@ -1,16 +1,47 @@
+// PDP, bag item and wishlist item price object
 export type Price = {
-  priceExclTaxes: number;
-  priceInclTaxes: number;
-  priceInclTaxesWithoutDiscount: number;
   discountExclTaxes: number;
   discountInclTaxes: number;
   discountRate: number;
+  formattedPrice: string;
+  formattedPriceWithoutCurrency: string;
+  formattedPriceWithoutDiscount: string;
+  formattedPriceWithoutDiscountAndCurrency: string;
+  priceExclTaxes: number;
+  priceInclTaxes: number;
+  priceInclTaxesWithoutDiscount: number;
+  tags: string[];
   taxesRate: number;
   taxesValue: number;
-  tags: string[];
+  taxType: string;
+};
+
+// PLP product price (scattered in `ProductSummary`, which is the name in Swagger)
+export type ProductSummaryPrice = {
+  currencyIsoCode: string;
   formattedPrice: string;
   formattedPriceWithoutDiscount: string;
+  price: number;
+  priceType: number;
+  priceWithoutDiscount: number;
+  promotionPercentage: number;
+};
+
+enum ProductPriceTypeEnum {
+  Min,
+  Max,
+}
+
+// PLP product `prices` individual object (`ProductSummaryTypedPrice` is the name in Swagger)
+export type ProductSummaryTypedPrice = {
+  formattedPrice: string;
   formattedPriceWithoutCurrency: string;
+  formattedPriceWithoutDiscount: string;
   formattedPriceWithoutDiscountAndCurrency: string;
-  taxType: string;
+  price: number;
+  priceWithoutDiscount: number;
+  promotionPercentage: number;
+  promotionType: string;
+  type: ProductPriceTypeEnum;
+  typeDescription: string;
 };
