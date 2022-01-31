d1e1e041d946def7537666fd7d2fb6de19671213
Gabriel Pires <gabriel.pires@farfetch.com>
2022-01-31 15:10:54 +0000

    chore(client): replace moxios with msw in merchantsLocations
    This replaces the usage of the `moxios` library by `msw` in the unit tests of the
    `merchantsLocations` chunk.
    
    Refs #18

--- packages/client/src/merchantsLocations/__tests__/getMerchantsLocations.test.ts
+++ packages/client/src/merchantsLocations/__tests__/getMerchantsLocations.test.ts
@@ -5,28 +5,22 @@ import {
 } from 'tests/__fixtures__/merchantsLocations';
 import client from '../../helpers/client';
 import fixtures from '../__fixtures__/getMerchantsLocations.fixtures';
-import moxios from 'moxios';
+import mswServer from '../../../tests/mswServer';
 
 describe('getMerchantsLocations', () => {
   const expectedConfig = undefined;
   const spy = jest.spyOn(client, 'get');
 
-  beforeEach(() => {
-    moxios.install(client);
-    jest.clearAllMocks();
-  });
-
-  afterEach(() => moxios.uninstall(client));
+  beforeEach(jest.clearAllMocks);
 
   it('should handle a client request successfully', async () => {
     const response = [mockMerchantLocation];
 
-    fixtures.success({
-      query: mockQuery,
-      response,
-    });
+    mswServer.use(fixtures.success(response));
+    expect.assertions(2);
 
-    await expect(getMerchantsLocations(mockQuery)).resolves.toBe(response);
+    await expect(getMerchantsLocations(mockQuery)).resolves.toEqual(response);
+
     expect(spy).toHaveBeenCalledWith(
       '/commerce/v1/merchantsLocations?merchantIds=1111%7C9359&merchantLocationIds=2222%7C1467',
       expectedConfig,
@@ -34,11 +28,11 @@ describe('getMerchantsLocations', () => {
   });
 
   it('should receive a client request error', async () => {
-    fixtures.failure({
-      query: mockQuery,
-    });
+    mswServer.use(fixtures.failure());
+    expect.assertions(2);
 
     await expect(getMerchantsLocations(mockQuery)).rejects.toMatchSnapshot();
+
     expect(spy).toHaveBeenCalledWith(
       '/commerce/v1/merchantsLocations?merchantIds=1111%7C9359&merchantLocationIds=2222%7C1467',
       expectedConfig,
