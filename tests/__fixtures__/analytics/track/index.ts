7f989e137746a22cee375193a243bb751ff2017b
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-02-04 15:16:38 +0000

    fix(react|redux): fixed GA4 mappings and improved wishlist middleware
    - Fixed mappings for product updated events.
    - Improved wishlist middleware to provide the value of
    `oldSize` and `oldQuantity` without depending from the caller of the
    action to provide those values through `meta` property.
    - Added unsynchronised events to snapshot tests.

--- tests/__fixtures__/analytics/track/index.ts
+++ tests/__fixtures__/analytics/track/index.ts
@@ -1,6 +1,7 @@
 import { eventTypes } from '@farfetch/blackout-analytics';
 import addressInfoAddedTrackData from './addressInfoAddedTrackData.fixtures';
 import changeScaleSizeGuideTrackData from './changeScaleSizeGuideTrackData.fixtures';
+import checkoutAbandonedTrackDataFixtures from './checkoutAbandonedTrackData.fixtures';
 import checkoutStartedTrackData from './checkoutStartedTrackData.fixtures';
 import checkoutStepCompletedTrackData from './checkoutStepCompletedTrackData.fixtures';
 import checkoutStepEditingTrackData from './checkoutStepEditingTrackData.fixtures';
@@ -12,6 +13,7 @@ import loginTrackData from './loginTrackData.fixtures';
 import orderCompletedTrackData from './orderCompletedTrackData.fixtures';
 import orderRefundedTrackData from './orderRefundedTrackData.fixtures';
 import paymentInfoAddedTrackData from './paymentInfoAddedTrackData.fixtures';
+import placeOrderStartedTrackDataFixtures from './placeOrderStartedTrackData.fixtures';
 import productAddedToCartTrackData from './productAddedToCartTrackData.fixtures';
 import productAddedToWishlistTrackData from './productAddedToWishlistTrackData.fixtures';
 import productClickedTrackData from './productClickedTrackData.fixtures';
@@ -21,6 +23,7 @@ import productRemovedFromWishlistTrackData from './productRemovedFromWishlistTra
 import productUpdatedTrackData from './productUpdatedTrackData.fixtures';
 import productUpdatedWishlistTrackData from './productUpdatedWishlistTrackData.fixtures';
 import productViewedTrackData from './productViewedTrackData.fixtures';
+import promocodeAppliedTrackDataFixtures from './promocodeAppliedTrackData.fixtures';
 import sameBillingAddressSelectedTrackData from './sameBillingAddressSelectedTrackData.fixtures';
 import selectContentTrackData from './selectContentTrackData.fixtures';
 import shareTrackData from './shareTrackData.fixtures';
@@ -31,16 +34,20 @@ import sortOptionChangedTrackData from './sortOptionChangedTrackData.fixtures';
 
 export default {
   [eventTypes.ADDRESS_INFO_ADDED]: addressInfoAddedTrackData,
+  [eventTypes.CHANGE_SCALE_SIZE_GUIDE]: changeScaleSizeGuideTrackData,
+  [eventTypes.CHECKOUT_ABANDONED]: checkoutAbandonedTrackDataFixtures,
   [eventTypes.CHECKOUT_STARTED]: checkoutStartedTrackData,
   [eventTypes.CHECKOUT_STEP_COMPLETED]: checkoutStepCompletedTrackData,
   [eventTypes.CHECKOUT_STEP_EDITING]: checkoutStepEditingTrackData,
   [eventTypes.CHECKOUT_STEP_VIEWED]: checkoutStepViewedTrackData,
   [eventTypes.FILTERS_APPLIED]: filtersAppliedTrackData,
   [eventTypes.FILTERS_CLEARED]: filtersClearedTrackData,
+  [eventTypes.INTERACT_CONTENT]: interactContentTrackData,
   [eventTypes.LOGIN]: loginTrackData,
   [eventTypes.ORDER_COMPLETED]: orderCompletedTrackData,
   [eventTypes.ORDER_REFUNDED]: orderRefundedTrackData,
   [eventTypes.PAYMENT_INFO_ADDED]: paymentInfoAddedTrackData,
+  [eventTypes.PLACE_ORDER_STARTED]: placeOrderStartedTrackDataFixtures,
   [eventTypes.PRODUCT_ADDED_TO_CART]: productAddedToCartTrackData,
   [eventTypes.PRODUCT_ADDED_TO_WISHLIST]: productAddedToWishlistTrackData,
   [eventTypes.PRODUCT_CLICKED]: productClickedTrackData,
@@ -48,17 +55,16 @@ export default {
   [eventTypes.PRODUCT_REMOVED_FROM_CART]: productRemovedFromCartTrackData,
   [eventTypes.PRODUCT_REMOVED_FROM_WISHLIST]:
     productRemovedFromWishlistTrackData,
+  [eventTypes.PRODUCT_UPDATED]: productUpdatedTrackData,
   [eventTypes.PRODUCT_UPDATED_WISHLIST]: productUpdatedWishlistTrackData,
   [eventTypes.PRODUCT_VIEWED]: productViewedTrackData,
+  [eventTypes.PROMOCODE_APPLIED]: promocodeAppliedTrackDataFixtures,
   [eventTypes.SAME_BILLING_ADDRESS_SELECTED]:
     sameBillingAddressSelectedTrackData,
   [eventTypes.SELECT_CONTENT]: selectContentTrackData,
-  [eventTypes.SHIPPING_INFO_ADDED]: shippingInfoAddedTrackData,
-  [eventTypes.SIGNUP_FORM_COMPLETED]: signupFormCompletedTrackData,
-  [eventTypes.SHIPPING_METHOD_ADDED]: shippingMethodAddedTrackData,
-  [eventTypes.SORT_OPTION_CHANGED]: sortOptionChangedTrackData,
-  [eventTypes.PRODUCT_UPDATED]: productUpdatedTrackData,
   [eventTypes.SHARE]: shareTrackData,
-  [eventTypes.CHANGE_SCALE_SIZE_GUIDE]: changeScaleSizeGuideTrackData,
-  [eventTypes.INTERACT_CONTENT]: interactContentTrackData,
+  [eventTypes.SHIPPING_INFO_ADDED]: shippingInfoAddedTrackData,
+  [eventTypes.SHIPPING_METHOD_ADDED]: shippingMethodAddedTrackData,
+  [eventTypes.SIGNUP_FORM_COMPLETED]: signupFormCompletedTrackData,
+  [eventTypes.SORT_OPTION_CHANGED]: sortOptionChangedTrackData,
 };
