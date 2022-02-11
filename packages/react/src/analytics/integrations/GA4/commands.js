097fc522e3debc1be7dd23c17ced5c24d5c37f69
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-02-11 12:50:55 +0000

    feat(react): add GA4 custom scroll event mappings
    - This adds mappings for the GA4 custom scroll event. It maps the
    `interact_content` event with scroll type
    and whose target is the document.body element.
    - Fixes `PRODUCT_UPDATED` event implementation that would not let the user to
    specify an override for that event.
    - Simplified implementation of the `commands` module in GA4 to avoid
    redundancy.

--- packages/react/src/analytics/integrations/GA4/commands.js
+++ packages/react/src/analytics/integrations/GA4/commands.js
@@ -4,7 +4,7 @@
  */
 import {
   eventTypes,
-  pageTypes,
+  interactionTypes,
   utils,
 } from '@farfetch/blackout-core/analytics';
 import { validationSchemaBuilder } from '../shared/validation/eventSchemas';
@@ -13,60 +13,17 @@ import ga4EventNameMapping, {
   InternalEventTypes,
 } from './eventMapping';
 
-const getCommandForEvent = data => {
+const genericCommandsBuilder = data => {
   const eventName = ga4EventNameMapping[data.event];
+
+  if (!eventName) {
+    return null;
+  }
+
   return [['event', eventName, getEventProperties(data.event, data)]];
 };
 
-export default {
-  [eventTypes.PRODUCT_ADDED_TO_CART]: getCommandForEvent,
-  [eventTypes.PRODUCT_REMOVED_FROM_CART]: getCommandForEvent,
-  [eventTypes.PAYMENT_INFO_ADDED]: getCommandForEvent,
-  [eventTypes.PRODUCT_ADDED_TO_WISHLIST]: getCommandForEvent,
-  [eventTypes.PRODUCT_REMOVED_FROM_WISHLIST]: getCommandForEvent,
-  [eventTypes.SHIPPING_INFO_ADDED]: getCommandForEvent,
-  [eventTypes.CHECKOUT_STARTED]: getCommandForEvent,
-  [eventTypes.ORDER_COMPLETED]: getCommandForEvent,
-  [eventTypes.ORDER_REFUNDED]: getCommandForEvent,
-  [pageTypes.SEARCH]: getCommandForEvent,
-  [eventTypes.SELECT_CONTENT]: getCommandForEvent,
-  [eventTypes.PRODUCT_CLICKED]: getCommandForEvent,
-  [eventTypes.PRODUCT_VIEWED]: getCommandForEvent,
-  [eventTypes.PRODUCT_LIST_VIEWED]: getCommandForEvent,
-  [pageTypes.BAG]: getCommandForEvent,
-  [pageTypes.WISHLIST]: getCommandForEvent,
-  [eventTypes.LOGIN]: getCommandForEvent,
-  [eventTypes.SIGNUP_FORM_COMPLETED]: getCommandForEvent,
-  [eventTypes.FILTERS_APPLIED]: getCommandForEvent,
-  [eventTypes.FILTERS_CLEARED]: getCommandForEvent,
-  [eventTypes.SHARE]: getCommandForEvent,
-  [eventTypes.CHECKOUT_STARTED]: getCommandForEvent,
-  [eventTypes.PLACE_ORDER_STARTED]: getCommandForEvent,
-  [eventTypes.CHECKOUT_ABANDONED]: getCommandForEvent,
-  [eventTypes.PROMOCODE_APPLIED]: getCommandForEvent,
-  [eventTypes.CHECKOUT_STEP_EDITING]: getCommandForEvent,
-  [eventTypes.ADDRESS_INFO_ADDED]: getCommandForEvent,
-  [eventTypes.SHIPPING_METHOD_ADDED]: getCommandForEvent,
-  [InternalEventTypes.PRODUCT_UPDATED.CHANGE_QUANTITY]: getCommandForEvent,
-  [InternalEventTypes.PRODUCT_UPDATED.CHANGE_SIZE]: getCommandForEvent,
-  [InternalEventTypes.PRODUCT_UPDATED.CHANGE_COLOUR]: getCommandForEvent,
-  [eventTypes.INTERACT_CONTENT]: getCommandForEvent,
-  [eventTypes.SIGNUP_NEWSLETTER]: getCommandForEvent,
-};
-
-// Schema used to validate the output of command functions
-export const commandListSchema = validationSchemaBuilder
-  .array()
-  .of(validationSchemaBuilder.array());
-
-// List of default non-interaction events
-export const nonInteractionEvents = {
-  [eventTypes.CHECKOUT_STEP_VIEWED]: true,
-  [eventTypes.PRODUCT_LIST_VIEWED]: true,
-  [eventTypes.PRODUCT_VIEWED]: true,
-};
-
-export const getProductUpdatedEventList = data => {
+const getProductUpdatedEventList = data => {
   const eventProperties = utils.getProperties(data);
   const dispatchGA4EventList = [];
 
@@ -95,4 +52,60 @@ export const getProductUpdatedEventList = data => {
 
   // return list of events which will be triggered
   return dispatchGA4EventList;
+};
+
+const productUpdatedEventCommandsBuilder = data => {
+  const internalEvents = getProductUpdatedEventList(data);
+
+  const commands = internalEvents.map(internalEvent =>
+    genericCommandsBuilder({ ...data, event: internalEvent })?.shift(),
+  );
+
+  return commands;
+};
+
+const interactContentEventCommandsBuilder = data => {
+  const eventProperties = data.properties;
+
+  if (
+    eventProperties.interactionType === interactionTypes.SCROLL &&
+    eventProperties.target === document.body
+  ) {
+    return genericCommandsBuilder({
+      ...data,
+      event: InternalEventTypes.PAGE_SCROLL,
+    });
+  }
+
+  return genericCommandsBuilder(data);
+};
+
+const specializedCommandsBuilderByEvent = {
+  [eventTypes.PRODUCT_UPDATED]: productUpdatedEventCommandsBuilder,
+  [eventTypes.INTERACT_CONTENT]: interactContentEventCommandsBuilder,
+};
+
+const defaultCommandsBuilder = data => {
+  const specializedEventCommandsBuilder =
+    specializedCommandsBuilderByEvent[data.event];
+
+  if (specializedEventCommandsBuilder) {
+    return specializedEventCommandsBuilder(data);
+  }
+
+  return genericCommandsBuilder(data);
+};
+
+export default defaultCommandsBuilder;
+
+// Schema used to validate the output of command functions
+export const commandListSchema = validationSchemaBuilder
+  .array()
+  .of(validationSchemaBuilder.array());
+
+// List of default non-interaction events
+export const nonInteractionEvents = {
+  [eventTypes.CHECKOUT_STEP_VIEWED]: true,
+  [eventTypes.PRODUCT_LIST_VIEWED]: true,
+  [eventTypes.PRODUCT_VIEWED]: true,
 };
