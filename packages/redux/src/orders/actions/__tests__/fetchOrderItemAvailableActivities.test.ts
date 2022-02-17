7b758ee2cdb244b473464375b70f62feadc945d8
jpklzm <jeanmenezes.nave@gmail.com>
2022-02-17 10:59:17 +0000

    feat(client|redux): add order activities endpoints

---
+++ packages/redux/src/orders/actions/__tests__/fetchOrderItemAvailableActivities.test.ts
@@ -0,0 +1,78 @@
+import { actionTypes } from '../..';
+import { fetchOrderItemAvailableActivities } from '..';
+import { getOrderItemAvailableActivities } from '@farfetch/blackout-client/orders';
+import { INITIAL_STATE } from '../../reducer';
+import { itemId, mockOrderDocumentsResponse } from 'tests/__fixtures__/orders';
+import { mockStore } from '../../../../tests';
+
+jest.mock('@farfetch/blackout-client/orders', () => ({
+  ...jest.requireActual('@farfetch/blackout-client/orders'),
+  getOrderItemAvailableActivities: jest.fn(),
+}));
+
+const ordersMockStore = (state = {}) =>
+  mockStore({ orders: INITIAL_STATE }, state);
+const orderId = '24BJKS';
+const expectedConfig = undefined;
+let store;
+
+describe('fetchOrderItemAvailableActivities() action creator', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    store = ordersMockStore();
+  });
+
+  it('should create the correct actions fetch when the get order item activities procedure fails', async () => {
+    const expectedError = new Error('fetch order item activities error');
+
+    getOrderItemAvailableActivities.mockRejectedValueOnce(expectedError);
+
+    expect.assertions(4);
+
+    try {
+      await store.dispatch(fetchOrderItemAvailableActivities(orderId, itemId));
+    } catch (error) {
+      expect(error).toBe(expectedError);
+      expect(getOrderItemAvailableActivities).toHaveBeenCalledTimes(1);
+      expect(getOrderItemAvailableActivities).toHaveBeenCalledWith(
+        orderId,
+        itemId,
+        expectedConfig,
+      );
+      expect(store.getActions()).toEqual(
+        expect.arrayContaining([
+          { type: actionTypes.FETCH_ORDER_ITEM_AVAILABLE_ACTIVITIES_REQUEST },
+          {
+            type: actionTypes.FETCH_ORDER_ITEM_AVAILABLE_ACTIVITIES_FAILURE,
+            payload: { error: expectedError },
+          },
+        ]),
+      );
+    }
+  });
+
+  it('should create the correct actions for when the fetch order item activities procedure is successful', async () => {
+    getOrderItemAvailableActivities.mockResolvedValueOnce(
+      mockOrderDocumentsResponse,
+    );
+
+    expect.assertions(4);
+
+    await store
+      .dispatch(fetchOrderItemAvailableActivities(orderId, itemId))
+      .then(clientResult => {
+        expect(clientResult).toEqual(mockOrderDocumentsResponse);
+      });
+
+    expect(getOrderItemAvailableActivities).toHaveBeenCalledTimes(1);
+    expect(getOrderItemAvailableActivities).toHaveBeenCalledWith(
+      orderId,
+      itemId,
+      expectedConfig,
+    );
+    expect(store.getActions()).toEqual([
+      { type: actionTypes.FETCH_ORDER_ITEM_AVAILABLE_ACTIVITIES_REQUEST },
+      { type: actionTypes.FETCH_ORDER_ITEM_AVAILABLE_ACTIVITIES_SUCCESS },
+    ]);
+  });
+});
