9071ee1a565f756a8edb96da1ad2e169cbaae226
João Ramalho Costa <joao.ramalhocosta@farfetch.com>
2022-03-14 09:49:18 +0000

    feat(react): update renderContent method
    Update renderContent method to adapt data for content tool.

--- packages/react/src/content/helpers/__tests__/renderContent.test.js
+++ packages/react/src/content/helpers/__tests__/renderContent.test.js
@@ -1,20 +1,40 @@
+import { cleanup, render } from '@testing-library/react';
+import {
+  mockContentToolData,
+  mockLegacyData,
+} from '../__fixtures__/renderContent.fixtures';
+import { registerComponent } from '../../components/Component';
+import React from 'react';
 import renderContent from '../renderContent';
 
-describe('renderContent', () => {
-  it('should render components list', () => {
-    const data = {
-      components: [
-        {
-          type: 'testComponent',
-          value: 'test',
-        },
-        {
-          type: 'text',
-          value: 'test',
-        },
-      ],
-    };
+const MockComponent = () => <p data-test="textElement">Content</p>;
 
-    expect(renderContent(data)).toMatchSnapshot();
+describe('renderContent', () => {
+  beforeEach(() => {
+    registerComponent('text', MockComponent);
+  });
+
+  afterEach(cleanup);
+
+  it('should not render any component if components are empty', () => {
+    const { queryByTestId } = render(renderContent({ components: [] }));
+
+    expect(queryByTestId('textElement')).not.toBeInTheDocument();
+  });
+
+  it('should render a component element with legacy data', () => {
+    const { getByTestId } = render(renderContent(mockLegacyData));
+
+    expect(getByTestId('textElement')).toBeInTheDocument();
+    expect(getByTestId('textElement')).toHaveTextContent('Content');
+  });
+
+  it('should render a component with content tool data structure', () => {
+    const { getByTestId } = render(
+      renderContent(mockContentToolData, 'active'),
+    );
+
+    expect(getByTestId('textElement')).toBeInTheDocument();
+    expect(getByTestId('textElement')).toHaveTextContent('Content');
   });
 });
--- packages/react/src/content/helpers/renderContent.js
+++ packages/react/src/content/helpers/renderContent.js
@@ -1,6 +1,10 @@
 import { Component } from '../components';
-import map from 'lodash/map';
 import React from 'react';
+
+const IS_CONTENT_TOOL = {
+  active: 'active',
+  inactive: 'inactive',
+};
 
 /**
  * Render an Editorial Content.
@@ -10,11 +14,26 @@ import React from 'react';
  * @memberof module:content/helpers
  *
  * @param {object} data - Data to render.
- * @param {Array}  data.components - Collection of components to render.
+ * @param {object[]} data.components - Collection of components to render.
+ * @param {'active'|'inactive'} isContentTool - Site key to identify if Content Tool is active or not.
  *
  * @returns {object} Rendered components.
  */
-export default ({ components }) =>
-  map(components, (component, key) => (
-    <Component component={component} key={key} />
-  ));
+const renderContent = (
+  { components },
+  isContentTool = IS_CONTENT_TOOL.inactive,
+) => {
+  if (components.length === 0) {
+    return null;
+  }
+
+  return components.map((component, key) => {
+    if (isContentTool === IS_CONTENT_TOOL.active && component.type === 'list') {
+      return renderContent(component, IS_CONTENT_TOOL.active);
+    }
+
+    return <Component component={component} key={key} />;
+  });
+};
+
+export default renderContent;
