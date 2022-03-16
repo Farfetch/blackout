0daeef9248102a46b49a1836817dd64ee2f07db8
João Ramalho Costa <joao.ramalhocosta@farfetch.com>
2022-03-16 14:55:10 +0000

    feat(react): update renderContent method
    Update renderContent method to adapt data for content tool.

---
+++ packages/react/src/contents/helpers/__tests__/renderContent.test.tsx
@@ -0,0 +1,38 @@
+import { cleanup, render } from '@testing-library/react';
+import {
+  mockContentToolData,
+  mockLegacyData,
+} from 'tests/__fixtures__/contents';
+import { registerComponent } from '../..';
+import React, { ReactElement } from 'react';
+import renderContent from '../renderContent';
+
+const MockTextComponent = (): ReactElement => (
+  <p data-test="textElement">Content</p>
+);
+
+describe('renderContent', () => {
+  beforeAll(() => {
+    registerComponent('text', MockTextComponent);
+  });
+
+  afterEach(cleanup);
+
+  it('should render a component element with legacy data', () => {
+    const { getByTestId } = render(renderContent(mockLegacyData));
+    const element = getByTestId('textElement');
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveTextContent('Content');
+  });
+
+  it('should render a component with content tool data structure', () => {
+    const { getByTestId } = render(
+      renderContent(mockContentToolData, 'active'),
+    );
+    const element = getByTestId('textElement');
+
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveTextContent('Content');
+  });
+});
--- packages/react/src/contents/helpers/renderContent.tsx
+++ packages/react/src/contents/helpers/renderContent.tsx
@@ -1,22 +1,41 @@
-import map from 'lodash/map';
-import React, { ReactElement } from 'react';
 import { Component } from '../components';
-import type { ComponentType } from '@farfetch/blackout-client/contents/types';
+import React, { ReactElement } from 'react';
+import type {
+  ComponentType,
+  ContentEntries,
+} from '@farfetch/blackout-client/contents/types';
+
+const IS_CONTENT_TOOL = {
+  active: 'active',
+  inactive: 'inactive',
+};
 
 /**
  * Render an Editorial Content.
  * Renders Editorial Content by going through all it's components and rendering them using the Editorial Component.
  *
- * @param {object} data - Data to render.
- * @param {Components[]} data.components - Collection of components to render.
+ * @function
+ * @memberof module:content/helpers
  *
- * @returns {ReactElement[]} Rendered components.
+ * @param {object} data - Data to render.
+ * @param {object[]} data.components - Collection of components to render.
+ * @param {'active'|'inactive'} isContentTool - Site key to identify if Content Tool is active or not.
+ *
+ * @returns {ReactElement} Rendered components.
  */
-const renderContent = ({
-  components
-}:{ components: ComponentType[] }): ReactElement[] =>
-  map(components, (component, key) => (
-    <Component component={component} key={key} />
-  ));
+const renderContent = (
+  { components }: { components: ContentEntries['components'] },
+  isContentTool = IS_CONTENT_TOOL.inactive,
+): ReactElement =>
+  components.map((component, key) => {
+    if (isContentTool === IS_CONTENT_TOOL.active && component.type === 'list') {
+      return renderContent(
+        component as { components: ComponentType[] },
+        IS_CONTENT_TOOL.active,
+      );
+    }
+
+    return <Component component={component} key={key} />;
+  }) as unknown as ReactElement;
 
 export default renderContent;
