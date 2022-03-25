17ff772cc8ae11ad113c62d42b566492ff836bd3
Ricardo Jorge Mota <ricardosilva.mota@farfetch.com>
2022-03-25 18:15:43 +0000

    feat(react): include new params hook useContentType
    Include new params in hook useContentType such as preview, to preview content

--- packages/react/src/content/hooks/useContentType.js
+++ packages/react/src/content/hooks/useContentType.js
@@ -19,10 +19,12 @@ import { useSelector } from 'react-redux';
  * @param {string | string[]} codes - List of codes that representing the content code (about-us|today-news|header|productId...).
  * @param {string} contentTypeCode - The custom content type unique code (e.g. 'careers').
  * @param {object} params - The target parameters that a content page is configured.
+ * @param {string} [params.environmentcode] - Query a content by a specific environmentcode (environmentcode:live|preview).
  * @param {string} params.countryCode - Query a content by a specific country (country:GB).
  * @param {string} params.cultureCode - Query a content by a specific language (language:en-GB).
  * @param {string} [params.benefits] - Query a content by is benefits (benefits:test).
  * @param {string} [params.contentzone] - Query a content by a specific content zone (contentzone:ROW).
+ * @param {string} [params.preview] - Query a content by a specific target preview guid.
  * @param {number} [pageSize=60] - Size of each page, as a number between 1 and 180. The default is 60.
  *
  * @returns {object} - Returns actions and selectors for custom content type data.
@@ -31,11 +33,13 @@ export default (codes, contentTypeCode, params, pageSize) => {
   const query = {
     ...(codes && { codes }),
     contentTypeCode,
-    environmentCode: process.env.WEB_APP_CONTENT_ENV || '',
+    environmentCode:
+      params?.environmentcode || process.env.WEB_APP_CONTENT_ENV || '',
     'target.country': params?.countryCode,
     'target.language': params?.cultureCode,
     'target.benefits': params?.benefits,
     'target.contentzone': params?.contentzone,
+    'target.preview': params?.preview,
     pageSize,
   };
   const contentTypeError = useSelector(state => getContentError(state, query));
