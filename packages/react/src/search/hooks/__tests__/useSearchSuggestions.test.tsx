a4cff187cb722e36c1d7228c8bfdd4cdb134e13b
Ana Pimentel <ana.pimentel@farfetch.com>
2022-04-28 14:27:53 +0100

    feat: add `useSearchSuggestions` hook
    This adds the `useSearchSuggestions` hook to the react package to facilitate
    the use of selectors and actions for search suggestions.

---
+++ packages/react/src/search/hooks/__tests__/useSearchSuggestions.test.tsx
@@ -0,0 +1,88 @@
+import { cleanup } from '@testing-library/react';
+import { mockSearchSuggestionsState } from 'tests/__fixtures__/search';
+import { mockStore } from '../../../../tests/helpers';
+import { Provider } from 'react-redux';
+import { renderHook } from '@testing-library/react-hooks';
+import { UseSearchSuggestionsReturn } from '../types';
+import React from 'react';
+import useSearchSuggestions from '../useSearchSuggestions';
+
+jest.mock('@farfetch/blackout-redux/search', () => ({
+  ...jest.requireActual('@farfetch/blackout-redux/search'),
+  fetchSearchSuggestions: jest.fn(() => ({ type: 'fetch' })),
+  resetSearchSuggestions: jest.fn(() => ({ type: 'reset' })),
+}));
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+
+const getRenderedHook = (
+  state = mockSearchSuggestionsState,
+): UseSearchSuggestionsReturn => {
+  const {
+    result: { current },
+  } = renderHook(() => useSearchSuggestions(), {
+    wrapper: props => <Provider store={mockStore(state)} {...props} />,
+  });
+
+  return current;
+};
+
+describe('useSearchSuggestions', () => {
+  beforeEach(jest.clearAllMocks);
+  afterEach(cleanup);
+
+  it('should return values correctly with initial state', () => {
+    const current = getRenderedHook();
+
+    expect(current).toStrictEqual({
+      error: expect.any(Object),
+      fetchSearchSuggestions: expect.any(Function),
+      isLoading: expect.any(Boolean),
+      resetSearchSuggestions: expect.any(Function),
+      suggestions: expect.any(Array),
+    });
+  });
+
+  it('should render in error state', () => {
+    const mockError = mockSearchSuggestionsState.search.suggestions.error;
+    const { error } = getRenderedHook(mockSearchSuggestionsState);
+
+    expect(error).toEqual(mockError);
+  });
+
+  it('should render in loading state', () => {
+    const { isLoading } = getRenderedHook({
+      search: {
+        suggestions: {
+          ...mockSearchSuggestionsState.search.suggestions,
+          isLoading: true,
+        },
+      },
+    });
+
+    expect(isLoading).toEqual(true);
+  });
+
+  describe('actions', () => {
+    it('should call `fetchSearchSuggestions` action', () => {
+      const { fetchSearchSuggestions } = getRenderedHook();
+
+      fetchSearchSuggestions({ query: 'query' });
+
+      expect(mockDispatch).toHaveBeenCalledWith({ type: 'fetch' });
+    });
+
+    it('should call `resetSearchSuggestions` action', () => {
+      const { resetSearchSuggestions } = getRenderedHook();
+
+      resetSearchSuggestions();
+
+      expect(mockDispatch).toHaveBeenCalledWith({ type: 'reset' });
+    });
+  });
+});
