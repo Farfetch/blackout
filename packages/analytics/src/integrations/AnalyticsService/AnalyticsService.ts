eae09d71fb793e5f40a5b76bd86a05e28895569e
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-05-02 10:53:06 +0100

    chore(blackout-analytics|blackout-client|blackout-react): transform analytics service to typescript
    - This converts AnalyticsService to typescript.

--- packages/analytics/src/integrations/AnalyticsService/AnalyticsService.ts
+++ packages/analytics/src/integrations/AnalyticsService/AnalyticsService.ts
@@ -1,24 +1,20 @@
 /* eslint-disable no-unused-vars */
 /**
- * Analytics service integration will send data to fps Analytics service.
+ * Analytics service integration will send data to Analytics service.
  *
  * @example <caption>Extending Analytics Service integration</caption>
  *
- * import { integrations } from '@farfetch/blackout-analytics';
+ * import \{ integrations \} from '\@farfetch/blackout-analytics';
  *
- * class ReactAnalyticsService extends integrations.AnalyticsService {
- *      setup(interval) {
+ * class ReactAnalyticsService extends integrations.AnalyticsService \{
+ *      setup(interval) \{
  *          // Create an interval that will handle the queue.
- *      }
+ *      \}
  *
- *      clearInterval() {
+ *      clearInterval() \{
  *          // Handle how the clearInterval should be done
- *      }
- * }
- *
- * @module AnalyticsService
- * @category Analytics
- * @subcategory Integrations
+ *      \}
+ * \}
  */
 
 import {
@@ -32,15 +28,27 @@ import { hashUserData } from '../../utils';
 import { postAnalytics } from '@farfetch/blackout-client/analyticsService';
 import get from 'lodash/get';
 import Integration from '../Integration';
+import type {
+  AnalyticsRequestPayload,
+  AnalyticsServiceOptions,
+} from './types/AnalyticsService.types';
+import type {
+  EventData,
+  LoadIntegrationEventData,
+  SetUserEventData,
+  StrippedDownAnalytics,
+  TrackTypesValues,
+} from '../..';
 
 /**
  * Analytics service integration.
- *
- * @private
- * @augments Integration
  */
-class AnalyticsService extends Integration {
-  constructor(options, loadData, analytics) {
+class AnalyticsService extends Integration<AnalyticsServiceOptions> {
+  constructor(
+    options: AnalyticsServiceOptions,
+    loadData: LoadIntegrationEventData,
+    analytics: StrippedDownAnalytics,
+  ) {
     super(options, loadData, analytics);
 
     this.initialize();
@@ -49,7 +57,7 @@ class AnalyticsService extends Integration {
   /**
    * Method to check if the integration is ready to be loaded.
    *
-   * @returns {boolean} If the integration is ready to be loaded.
+   * @returns If the integration is ready to be loaded.
    */
   static shouldLoad() {
     return true;
@@ -58,41 +66,28 @@ class AnalyticsService extends Integration {
   /**
    * List of events that will be stored for a period of time until they are processed.
    */
-  queue = [];
+  queue: AnalyticsRequestPayload[] = [];
 
   /**
    * Interval that will be stored here.
    */
-  interval = undefined;
-
-  /**
-   * Method used to create a new AnalyticsService instance by analytics.
-   *
-   * @param {object} options - Integration options.
-   * @param {object} loadData - Analytics's load event data.
-   * @param {object} analytics - Analytics instance stripped down with only helpers.
-   *
-   * @returns {AnalyticsService} An instance of AnalyticsService class.
-   */
-  static createInstance(options, loadData, analytics) {
-    return new AnalyticsService(options, loadData, analytics);
-  }
+  interval?: NodeJS.Timeout = undefined;
 
   /**
    * Handles onSetUser event by sending the event data to Analytics service.
    *
-   * @param {object} data - Event data provided by analytics.
+   * @param data - Event data provided by analytics.
    */
-  onSetUser(data) {
+  onSetUser(data: SetUserEventData) {
     this.addToQueue(data);
   }
 
   /**
    * Post all trackings to Analytics service.
    *
-   * @param {object} data - Event data provided by analytics.
+   * @param data - Event data provided by analytics.
    */
-  track(data) {
+  track(data: EventData<TrackTypesValues>) {
     this.addToQueue(data);
   }
 
@@ -123,14 +118,12 @@ class AnalyticsService extends Integration {
 
   /**
    * Function that will create and store the interval.
-   * This method is a placeholder and it should be implemented by @farfetch/blackout-react/analytics and @farfetch/blackout-react-native-analytics.
+   * This method is a placeholder and it should be implemented by \@farfetch/blackout-react/analytics and \@farfetch/blackout-react-native-analytics.
    *
-   * @param {number} interval - The interval in milliseconds for the queue flush.
-   *
-   * @abstract
+   * @param interval - The interval in milliseconds for the queue flush.
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setup(interval) {
+  setup(interval: number): void {
     throw new Error(
       "This method should be implemented by the subclass. If you are getting this error, make sure you are using the correct integration from '@farfetch/blackout-client/react' or '@farfetch/blackout-react-native-analytics'.",
     );
@@ -138,14 +131,12 @@ class AnalyticsService extends Integration {
 
   /**
    * Controls the queue by flushing it when a new page is tracked.
-   * This method is a placeholder and it should be implemented by @farfetch/blackout-react/analytics and @farfetch/blackout-react-native-analytics.
+   * This method is a placeholder and it should be implemented by \@farfetch/blackout-react/analytics and \@farfetch/blackout-react-native-analytics.
    *
-   * @param {object} data - Event data provided by analytics.
-   *
-   * @abstract
+   * @param data - Event data provided by analytics.
    */
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  controlQueue(data) {
+  controlQueue(data: EventData<TrackTypesValues> | SetUserEventData): void {
     throw new Error(
       "This method should be implemented by the subclass. If you are getting this error, make sure you are using the correct integration from '@farfetch/blackout-client/react' or '@farfetch/blackout-react-native-analytics'.",
     );
@@ -154,9 +145,9 @@ class AnalyticsService extends Integration {
   /**
    * Adds events to the queue.
    *
-   * @param {object} data - Event data provided by analytics.
+   * @param data - Event data provided by analytics.
    */
-  addToQueue(data) {
+  addToQueue(data: EventData<TrackTypesValues> | SetUserEventData) {
     const protectedUserData = hashUserData(data.user);
 
     const eventData = {
@@ -193,9 +184,7 @@ class AnalyticsService extends Integration {
   }
 
   /**
-   * This method is a placeholder and it should be implemented by @farfetch/blackout-react/analytics and @farfetch/blackout-react-native-analytics.
-   *
-   * @abstract
+   * This method is a placeholder and it should be implemented by \@farfetch/blackout-react/analytics and \@farfetch/blackout-react-native-analytics.
    */
   clearInterval() {
     throw new Error(
@@ -206,9 +195,9 @@ class AnalyticsService extends Integration {
   /**
    * Post data to Analytics service specifying the version and type of data.
    *
-   * @param {object} data - Event data provided by analytics.
+   * @param data - Event data provided by analytics.
    */
-  postDataToAnalyticsService(data) {
+  postDataToAnalyticsService(data: AnalyticsRequestPayload[]) {
     postAnalytics(data);
   }
 }
--- packages/react/src/analytics/integrations/AnalyticsService/__tests__/AnalyticsService.test.ts
+++ packages/react/src/analytics/integrations/AnalyticsService/__tests__/AnalyticsService.test.ts
@@ -1,10 +1,22 @@
-import { integrations, trackTypes } from '@farfetch/blackout-analytics';
+import {
+  integrations,
+  StrippedDownAnalytics,
+  trackTypes,
+} from '@farfetch/blackout-analytics';
+import { loadIntegrationData } from 'tests/__fixtures__/analytics';
 import ReactAnalyticsService from '../AnalyticsService';
+import type AnalyticsService from '..';
+
+const strippedDownAnalytics = {} as StrippedDownAnalytics;
 
 describe('ReactAnalyticsService', () => {
   describe('integration', () => {
     it('Should create an instance of this integration', () => {
-      const instance = ReactAnalyticsService.createInstance();
+      const instance = ReactAnalyticsService.createInstance(
+        {},
+        loadIntegrationData,
+        strippedDownAnalytics,
+      );
 
       expect(instance).toBeInstanceOf(ReactAnalyticsService);
       expect(instance).toBeInstanceOf(integrations.AnalyticsService);
@@ -13,7 +25,11 @@ describe('ReactAnalyticsService', () => {
 
   describe('clearInterval method', () => {
     it('Should clear the interval when calling .clearInterval()', () => {
-      const instance = ReactAnalyticsService.createInstance();
+      const instance = ReactAnalyticsService.createInstance(
+        {},
+        loadIntegrationData,
+        strippedDownAnalytics,
+      ) as AnalyticsService;
 
       expect(instance.interval).toBeDefined();
 
@@ -24,17 +40,25 @@ describe('ReactAnalyticsService', () => {
   });
 
   it('Should flush the queue when a page track occurs', () => {
-    const instance = ReactAnalyticsService.createInstance();
+    const instance = ReactAnalyticsService.createInstance(
+      {},
+      loadIntegrationData,
+      strippedDownAnalytics,
+    ) as AnalyticsService;
     const flushSpy = jest.spyOn(instance, 'flushQueue');
 
+    // @ts-expect-error
     instance.track({});
+    // @ts-expect-error
     instance.track({});
+    // @ts-expect-error
     instance.track({});
 
     expect(instance.queue).toHaveLength(3);
 
     expect(flushSpy).not.toHaveBeenCalled();
 
+    // @ts-expect-error
     instance.track({ type: trackTypes.PAGE });
 
     expect(flushSpy).toHaveBeenCalled();
