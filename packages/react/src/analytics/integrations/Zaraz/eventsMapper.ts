ff927461a1d18127d0c0bed51ccae3ee9c48982c
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-05-20 17:18:00 +0100

    feat(blackout-react): add Zaraz integration
    - This adds the Zaraz integration to analytics, with support for running
    behind a proxy server.

---
+++ packages/react/src/analytics/integrations/Zaraz/eventsMapper.ts
@@ -0,0 +1,4 @@
+import type { EventsMapper } from './types/types';
+
+// To be added...
+export default {} as EventsMapper;
---
+++ packages/react/src/analytics/integrations/Zaraz/types/types.ts
@@ -0,0 +1,23 @@
+import type {
+  EventData,
+  IntegrationOptions,
+  TrackTypesValues,
+} from '@farfetch/blackout-analytics';
+import type {
+  OPTION_EVENTS_MAPPER,
+  OPTION_ZARAZ_INIT_SCRIPT_ENDPOINT,
+} from '../constants';
+
+export type ZarazEventData = [
+  'ecommerce' | 'track',
+  Record<string, string | number | boolean | undefined | null>,
+];
+
+type EventMapper = (data: EventData<TrackTypesValues>) => ZarazEventData;
+
+export type EventsMapper = Record<string, EventMapper>;
+
+export interface ZarazIntegrationOptions extends IntegrationOptions {
+  [OPTION_ZARAZ_INIT_SCRIPT_ENDPOINT]?: string;
+  [OPTION_EVENTS_MAPPER]?: EventsMapper;
+}
