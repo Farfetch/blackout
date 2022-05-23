4923762d7437a372f940634e8668babc63195df8
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-05-23 18:02:30 +0100

    fix(blackout-react): use env instead of host
    - This changes the way local testing detection to use the NODE_ENV
    value in BUILD_CONTEXT or a dedicated option instead of relying on
    the hostname.

--- packages/react/src/analytics/integrations/Zaraz/constants.ts
+++ packages/react/src/analytics/integrations/Zaraz/constants.ts
@@ -2,6 +2,7 @@ export const INTERNAL_ZARAZ_EVENT = '__ap_zaraz_load__';
 export const URL_HOST_REGEX = new RegExp('https?:\\/\\/([^\\/]*)', 'i');
 export const ZARAZ_REQUEST_REGEX = new RegExp('zaraz', 'i');
 
-export const OPTION_ZARAZ_INIT_SCRIPT_ENDPOINT = 'initScriptEndpoint';
+export const OPTION_ENVIRONMENT = 'environment';
 export const OPTION_EVENTS_MAPPER = 'eventsMapper';
+export const OPTION_ZARAZ_INIT_SCRIPT_ENDPOINT = 'initScriptEndpoint';
 export const DEFAULT_ZARAZ_INIT_SCRIPT_ENDPOINT = '/cdn-cgi/zaraz/i.js';
