5b9a58a24c19c4196eeab78df7102111508fa817
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-05-25 15:49:48 +0100

    feat(blackout-react): add product added to cart/wishlist mappings in Zaraz
    - This adds mappings for "Product Added to Cart" and "Product Added to
    Wishlist" events in Zaraz integration.
    - Fixed some leaks in Zaraz testing file.
    - Fixed unnecessary console.error calls when an event that does not
    contain a mapper is tracked.

--- packages/react/src/analytics/integrations/Zaraz/eventsMapper.js
+++ packages/react/src/analytics/integrations/Zaraz/eventsMapper.js
@@ -1,2 +1,31 @@
-// To be added...
-export default {};
+import { eventTypes } from '@farfetch/blackout-core/analytics';
+
+export default {
+  [eventTypes.PRODUCT_ADDED_TO_CART]: data => {
+    const eventProperties = data.properties;
+
+    return [
+      'ecommerce',
+      {
+        currency: eventProperties.currency,
+        name: eventProperties.name,
+        price: eventProperties.price,
+        product_id: eventProperties.id,
+      },
+    ];
+  },
+  [eventTypes.PRODUCT_ADDED_TO_WISHLIST]: data => {
+    const eventProperties = data.properties;
+
+    return [
+      'ecommerce',
+      {
+        category: eventProperties.category,
+        currency: eventProperties.currency,
+        name: eventProperties.name,
+        price: eventProperties.price,
+        product_id: eventProperties.id,
+      },
+    ];
+  },
+};
