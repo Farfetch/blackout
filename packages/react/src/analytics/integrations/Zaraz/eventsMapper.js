4eeed6dbfb303599cd48f9d24eb779f2bb894204
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-05-27 15:46:18 +0100

    feat(blackout-react): add product viewed/search mappings in Zaraz
    - This adds mappings for "Product Viewed" and "Search"
    events in Zaraz integration.
    - Force transforming product ids to a string in Zaraz events.

--- packages/react/src/analytics/integrations/Zaraz/eventsMapper.js
+++ packages/react/src/analytics/integrations/Zaraz/eventsMapper.js
@@ -1,4 +1,4 @@
-import { eventTypes } from '@farfetch/blackout-core/analytics';
+import { eventTypes, pageTypes } from '@farfetch/blackout-core/analytics';
 
 export default {
   [eventTypes.PRODUCT_ADDED_TO_CART]: data => {
@@ -10,7 +10,7 @@ export default {
         currency: eventProperties.currency,
         name: eventProperties.name,
         price: eventProperties.price,
-        product_id: eventProperties.id,
+        product_id: `${eventProperties.id}`, // Zaraz's product_id is a string
       },
     ];
   },
@@ -24,7 +24,35 @@ export default {
         currency: eventProperties.currency,
         name: eventProperties.name,
         price: eventProperties.price,
-        product_id: eventProperties.id,
+        product_id: `${eventProperties.id}`, // Zaraz's product_id is a string
+      },
+    ];
+  },
+  [eventTypes.PRODUCT_VIEWED]: data => {
+    const eventProperties = data.properties;
+
+    return [
+      'ecommerce',
+      {
+        currency: eventProperties.currency,
+        name: eventProperties.name,
+        price: eventProperties.price,
+        product_id: `${eventProperties.id}`, // Zaraz's product_id is a string
+      },
+    ];
+  },
+  [pageTypes.SEARCH]: data => {
+    const eventProperties = data.properties;
+
+    return [
+      'ecommerce',
+      {
+        price: 0, // This was required by Zaraz
+        currency: eventProperties.currency,
+        products: eventProperties.products?.map(product => ({
+          product_id: `${product.id}`, // Zaraz's product_id is a string
+        })),
+        query: eventProperties.searchQuery,
       },
     ];
   },
