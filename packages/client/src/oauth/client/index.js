b8d37fac5dca51dffdc2b0fe7c437de77c2f9a8c
Andre Costa <andre.araujocosta@farfetch.com>
2022-06-02 17:01:29 +0100

    chore(client): remove oauth files
    - Remove OAuth files.

--- packages/client/src/oauth/client/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * OAuth clients.
- *
- * @module oAuth/client
- * @category oAuth
- * @subcategory Clients
- */
-export { default as postOAuthToken } from './postOAuthToken';
-export { default as oAuthInterceptor } from './oAuthInterceptor';
