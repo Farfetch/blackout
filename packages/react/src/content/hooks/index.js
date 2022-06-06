8a5828b7b68b697692469b3793ee3cc4c5f045b3
Ricardo Jorge Mota <ricardosilva.mota@farfetch.com>
2022-06-06 12:56:14 +0100

    feat(core): add new client contentpages
    Create new client and redux to new endpoint contentPages

--- packages/react/src/content/hooks/index.js
+++ packages/react/src/content/hooks/index.js
@@ -6,6 +6,7 @@
  * @subcategory hooks
  */
 
+export { default as useContentPage } from './useContentPage';
 export { default as useCommercePage } from './useCommercePage';
 export { default as useContentType } from './useContentType';
 export { default as useNavbars } from './useNavbars';
