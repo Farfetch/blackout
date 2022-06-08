b69abca9a58635f8e41d97150bf62801cf85713e
Andre Costa <andre.araujocosta@farfetch.com>
2022-06-08 17:40:08 +0100

    feat(client): new reset password endpoint
    - New reset password endpoint.

--- packages/client/src/authentication/__fixtures__/postPasswordRecover.fixtures.ts
+++ packages/client/src/authentication/__fixtures__/postPasswordRecover.fixtures.ts
@@ -2,12 +2,12 @@ import moxios from 'moxios';
 
 export default {
   success: () => {
-    moxios.stubRequest('/api/account/v1/users/passwordrecover', {
+    moxios.stubRequest('/api/legacy/v1/account/password/retrieve', {
       status: 200,
     });
   },
   failure: () => {
-    moxios.stubRequest('/api/account/v1/users/passwordrecover', {
+    moxios.stubRequest('/api/legacy/v1/account/password/retrieve', {
       response: 'stub error',
       status: 404,
     });
