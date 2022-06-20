5398c8ac92594e391ab93b06b24e55efc7083225
Giulia Mummolo <giulia.mummolo@farfetch.com>
2022-06-20 15:19:56 +0100

    chore: replace PascalCase with camelCase

--- packages/redux/src/entities/types/subscriptionPackage.types.ts
+++ packages/redux/src/entities/types/subscriptionPackage.types.ts
@@ -1,5 +1,7 @@
 import type { SubscriptionPackage } from '@farfetch/blackout-client/subscriptions/types';
 
+export type SubscriptionPackageEntity = SubscriptionPackage;
+
 export type SubscriptionPackagesEntity =
   | Record<SubscriptionPackage['id'], SubscriptionPackage>
   | undefined;
