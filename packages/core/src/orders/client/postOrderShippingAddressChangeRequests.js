476b0a99cd7d5e2f44ab3b55012781c7f5d82254
Jean Menezes <jean.menezes@farfetch.com>
2022-07-12 20:07:46 +0100

    feat(core|redux): add order change address endpoints

---
+++ packages/core/src/orders/client/postOrderShippingAddressChangeRequests.js
@@ -0,0 +1,29 @@
+import adaptOrderDetails from './helpers/adaptOrderDetails';
+import client, { adaptError } from '../../helpers/client';
+import join from 'proper-url-join';
+
+/**
+ * Method responsible for change the shipping address of the order.
+ *
+ * @function postOrderShippingAddressChangeRequests
+ * @memberof module:orders/client
+ *
+ * @param {string} orderId - The orderID to get the details.
+ * @param {object} data - The change of the shipping address.
+ * @param {object} [config] - Custom configurations to send to the client
+ * instance (axios).
+ *
+ * @returns {Promise} Promise that will resolve when the call to
+ * the endpoint finishes.
+ */
+export default (orderId, data, config) =>
+  client
+    .post(
+      join('/account/v1/orders', orderId, 'shippingAddressChangeRequests'),
+      data,
+      config,
+    )
+    .then(response => adaptOrderDetails(response.data))
+    .catch(error => {
+      throw adaptError(error);
+    });
