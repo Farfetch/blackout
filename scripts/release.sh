04c4f28982b381fe2d33d9cf45bacb09836eba48
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2022-08-04 12:09:13 +0100

    chore: add main branch to release script
    - This adds main branch to the release script to enable
    releases when the next branch is merged into main.

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,17 +3,17 @@
 git remote set-url origin "https://${GITHUB_TOKEN}@github.com/Farfetch/blackout.git"
 
 SOURCE_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+CONTENTS="--contents dist"
 
-if [[ ${SOURCE_BRANCH_NAME} = main ]]; then
-    npx lerna publish --conventional-commits --message "${PUBLISH_COMMIT_MESSAGE}" --no-verify-access --yes
-else
+if [[ ${SOURCE_BRANCH_NAME} = main || ${SOURCE_BRANCH_NAME} = next ]]; then
+    yarn release:build
+
     if [[ ${SOURCE_BRANCH_NAME} = next ]]; then
-        PRE_ID=next
-        CONTENTS="--contents dist"
-        yarn release:build
-    else
-        PRE_ID=rc
+        PRE_ID_NAME='next'
+        PRE_ID="--preid ${PRE_ID_NAME}"
+        PRE_DIST_TAG="--pre-dist-tag ${PRE_ID_NAME}"
+        CONVENTIONAL_PRERELEASE="--conventional-prerelease"
     fi
-    
-    npx lerna publish --conventional-commits --conventional-prerelease --no-verify-access --preid ${PRE_ID} --pre-dist-tag ${PRE_ID}  --message "${PUBLISH_COMMIT_MESSAGE}"  --yes ${CONTENTS}
+
+    npx lerna publish --conventional-commits --message "${PUBLISH_COMMIT_MESSAGE}" --no-verify-access --yes ${CONTENTS} ${PRE_ID} ${PRE_DIST_TAG} ${CONVENTIONAL_PRERELEASE}
 fi
