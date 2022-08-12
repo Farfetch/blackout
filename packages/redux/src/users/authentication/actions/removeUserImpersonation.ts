b2eb02e9661d0863d7b4f90f6872d4291b990ee0
Jhair Abreu <jhair.abreu@farfetch.com>
2022-08-12 16:17:13 +0100

    chore(client|redux): remove deprecated userImpersonation operations
    BREAKING CHANGES:
    - Removed `userImpersonations` operations from client and redux packages, as they are deprecated.
    The same thing can now be done using the `postToken` client by adjusting request body.
    In summary, the following is no longer available:
    - deleteUserImpersonation client
    - postUserImpersonation client
    - isUserImpersonationLoading selector
    - getUserImpersonationError selector
    - getUserImpersonationResult selector
    - createUserImpersonation action and action types
    - removeUserImpersonation action and action types

--- packages/redux/src/users/authentication/actions/removeUserImpersonation.ts
+++
@@ -1,7 +0,0 @@
-import { deleteUserImpersonation } from '@farfetch/blackout-client';
-import { removeUserImpersonationFactory } from './factories';
-
-/**
- * Deletes an user impersonation.
- */
-export default removeUserImpersonationFactory(deleteUserImpersonation);
