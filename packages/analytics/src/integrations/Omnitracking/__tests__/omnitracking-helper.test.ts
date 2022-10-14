6aba34a1cb81f9ca9f44eed31ac93d5dfb7c12fd
Rui Nunes <ruifcnsilva@gmail.com>
2022-10-14 15:05:39 +0100

    feat(analytics): add omnitracking's shipping and payment mappings

--- packages/analytics/src/integrations/Omnitracking/__tests__/omnitracking-helper.test.ts
+++ packages/analytics/src/integrations/Omnitracking/__tests__/omnitracking-helper.test.ts
@@ -1,6 +1,7 @@
 import {
   generatePaymentAttemptReferenceId,
   getCheckoutEventGenericProperties,
+  getCommonCheckoutStepTrackingData,
   getDeliveryInformationDetails,
   getPageEventFromLocation,
   getPlatformSpecificParameters,
@@ -135,5 +136,23 @@ describe('getDeliveryInformationDetails', () => {
         } as Record<string, unknown>,
       } as EventData<TrackTypesValues>),
     ).toEqual('{"courierType":"sample"}');
+  });
+});
+
+describe('getCommonCheckoutStepTrackingData', () => {
+  it('should obtain common checkout parameters', () => {
+    expect(
+      getCommonCheckoutStepTrackingData({
+        properties: {
+          step: 2,
+          deliveryType: 'sample',
+          interactionType: 'click',
+        } as Record<string, unknown>,
+      } as EventData<TrackTypesValues>),
+    ).toEqual({
+      checkoutStep: 2,
+      deliveryInformationDetails: '{"courierType":"sample"}',
+      interactionType: 'click',
+    });
   });
 });
