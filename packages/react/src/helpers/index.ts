4369ca128a0e048db32925cc715069d2b8309a9a
Pedro Fernandes <pedro.barreiro@farfetch.com>
2023-01-12 11:19:20 +0000

    feat(react): add password strength helper
    Added a new function that validates the password
    and provides the relevant errors to display the
    necessary feedback to the user.

--- packages/react/src/helpers/index.ts
+++ packages/react/src/helpers/index.ts
@@ -1,5 +1,17 @@
 import * as headers from './headers';
 import getTimeInMinutes from './getTimeInMinutes';
 import usePrevious from './usePrevious';
+import validatePassword, {
+  PasswordValidationErrors,
+  PasswordValidationErrorsConstants,
+  PasswordValidationResult,
+} from './validatePassword';
 
-export { getTimeInMinutes, headers, usePrevious };
+export {
+  getTimeInMinutes,
+  headers,
+  usePrevious,
+  validatePassword,
+  PasswordValidationErrorsConstants,
+};
+export type { PasswordValidationErrors, PasswordValidationResult };
