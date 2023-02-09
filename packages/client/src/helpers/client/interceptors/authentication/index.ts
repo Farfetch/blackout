85c7745b523581612fa95f577bf962ffde05d9f6
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2023-02-09 18:28:51 +0000

    chore: reintroduce authentication interceptor
    This revers the commit that removed authentication interceptor as it is
    needed for mobile apps. When we find another place for it, we will
    remove it again.

---
+++ packages/client/src/helpers/client/interceptors/authentication/index.ts
@@ -0,0 +1,7 @@
+export * from './errors';
+export { TokenKinds, TokenData } from './token-providers';
+export type { ITokenData, TokenContext } from './token-providers';
+export { default as AuthenticationConfigOptions } from './AuthenticationConfigOptions';
+export { default as AuthenticationTokenManager } from './AuthenticationTokenManager';
+export * from './defaults';
+export * from './types';
