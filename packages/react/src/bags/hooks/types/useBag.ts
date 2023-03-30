eb448cf1a61bd4af1f3d5056f2f40ff881b2451f
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2023-03-30 18:04:51 +0100

    feat: add config parameter to `useBag` and `useBagItem` actions
    This adds a config parameter to all actions that were missing it from
    `useBag` and `useBagItem` actions. Also, removed the
    `HandleAddOrUpdateItem` from the exported types as it is an internal
    type.

--- packages/react/src/bags/hooks/types/useBag.ts
+++ packages/react/src/bags/hooks/types/useBag.ts
@@ -1,33 +1,4 @@
-import type {
-  BagItemActionMetadata,
-  BagItemDenormalized,
-  CustomAttributesAdapted,
-  ProductEntityDenormalized,
-  SizeAdapted,
-} from '@farfetch/blackout-redux';
 import type { Config, GetBagQuery } from '@farfetch/blackout-client';
-
-export type HandleAddOrUpdateItem = (
-  {
-    customAttributes,
-    from,
-    product,
-    productAggregatorId,
-    quantity,
-    size,
-  }: {
-    customAttributes?: CustomAttributesAdapted | string;
-    from?: string;
-    product: ProductEntityDenormalized;
-    productAggregatorId?: Exclude<
-      BagItemDenormalized['productAggregator'],
-      null
-    >['id'];
-    quantity: number;
-    size: SizeAdapted;
-  },
-  metadata?: BagItemActionMetadata,
-) => Promise<void>;
 
 export type UseBagOptions = {
   enableAutoFetch?: boolean;
