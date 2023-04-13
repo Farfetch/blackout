17ba87c80ca8ba698406c2d186f8fe6149777ca0
Daniel Bento <danielbento92@gmail.com>
2023-04-13 09:37:20 +0100

    feat(analytics|react): add package version to analytics next
    - Add parameter to each integration to fill package versions on omni and ga4 requests.
    - Omnitracking, are missing update contract that would allow to send
    this new parameter on final request.

--- packages/react/src/analytics/analytics.ts
+++ packages/react/src/analytics/analytics.ts
@@ -1,11 +1,21 @@
+import { get } from 'lodash-es';
 import Analytics, {
   TrackTypes as analyticsTrackTypes,
+  type ContextData,
   type EventContextData,
+  type EventData,
   type EventProperties,
   type IntegrationRuntimeData,
   PlatformTypes,
+  type TrackTypesValues,
 } from '@farfetch/blackout-analytics';
 import webContext from './context.js';
+
+const {
+  name: PACKAGE_NAME,
+  version: PACKAGE_VERSION,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require('../../package.json');
 
 /**
  * Analytics facade for web applications. Refer to \@farfetch/blackout-analytics
@@ -44,7 +54,7 @@ class AnalyticsWeb extends Analytics {
     if (this.currentPageCallData) {
       const { event, properties } = this.currentPageCallData;
 
-      const pageEventData = await super.getTrackEventData(
+      const pageEventData = await this.getTrackEventData(
         analyticsTrackTypes.PAGE,
         event,
         properties,
@@ -53,6 +63,59 @@ class AnalyticsWeb extends Analytics {
       super.forEachIntegrationSafe(loadedIntegrations, integration =>
         integration.track(pageEventData),
       );
+    }
+  }
+
+  /**
+   * Gets event data for a track event.
+   *
+   * @param type         - Type of the event being called.
+   * @param event        - Name of the event from analytics.track call.
+   * @param properties   - Event properties from analytics.track call.
+   * @param eventContext - Context data that is specific for this event.
+   *
+   * @returns - Track event data to be sent to integrations.
+   */
+  override async getTrackEventData(
+    type: TrackTypesValues,
+    event: string,
+    properties?: EventProperties,
+    eventContext?: ContextData,
+  ): Promise<EventData<TrackTypesValues>> {
+    this.processContext(eventContext);
+
+    return await super.getTrackEventData(type, event, properties, eventContext);
+  }
+
+  /**
+   * Getter for the context object.
+   *
+   * @param key - Key to retrieve from the context. If not specified, will return the whole data stored
+   *              in the context.
+   *
+   * @returns Value for the key in context or the whole context data if key is not specified.
+   */
+  override context(): Promise<ContextData>;
+  override context(key: string): Promise<unknown>;
+  override async context(key?: string) {
+    const context = await super.context();
+
+    this.processContext(context);
+
+    return key ? get(context, key) : context;
+  }
+
+  /**
+   * Process context to append context features related with this package.
+   *
+   * @param context - Context data that is specific for this event.
+   */
+  processContext(context?: ContextData) {
+    if (context) {
+      context.library = {
+        name: PACKAGE_NAME,
+        version: `${context.library.name}@${context.library.version};${PACKAGE_NAME}@${PACKAGE_VERSION};`,
+      };
     }
   }
 
