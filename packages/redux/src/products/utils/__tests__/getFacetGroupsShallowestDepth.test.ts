e3aa7882d8b4af2caaacd75f08854aab4e24d080
Bruno Alexandre Oliveira <Bruno.A.Oliveira@farfetch.com>
2023-04-17 12:00:30 +0100

    chore: rename facet groups selectors
    This renames the selectors `getMaxDepth` and `getShallowestDepth`
    to `getFacetGroupsMaxDepth` and `getFacetGroupsShallowestDepth`
    respectively to improve consistency. Also, the
    `getFacetGroupsShallowestDepth` is now being exported from the
    index as well.
    
    BREAKING CHANGE: The selectors `getMaxDepth` and `getShallowestDepth`
    were renamed to `getFacetGroupsMaxDepth` and `getFacetGroupsShallowestDepth`
    respectively.

--- packages/redux/src/products/utils/__tests__/getFacetGroupsShallowestDepth.test.ts
+++ packages/redux/src/products/utils/__tests__/getFacetGroupsShallowestDepth.test.ts
@@ -2,11 +2,11 @@ import {
   mockFacetGroupsNormalized,
   mockShallowestDepth,
 } from 'tests/__fixtures__/products/index.mjs';
-import getShallowestDepth from '../getShallowestDepth.js';
+import getFacetGroupsShallowestDepth from '../getFacetGroupsShallowestDepth.js';
 
-describe('getShallowestDepth()', () => {
+describe('getFacetGroupsShallowestDepth()', () => {
   it('should return the shallowest depth founded in facet groups received', () => {
-    expect(getShallowestDepth(mockFacetGroupsNormalized)).toBe(
+    expect(getFacetGroupsShallowestDepth(mockFacetGroupsNormalized)).toBe(
       mockShallowestDepth,
     );
   });
--- packages/redux/src/products/utils/index.ts
+++ packages/redux/src/products/utils/index.ts
@@ -5,6 +5,7 @@ export { default as buildFacetTree } from './buildFacetTree.js';
 export { default as buildSetFiltersQueryParams } from './buildSetFiltersQueryParams.js';
 export { default as buildUnsetFiltersQueryParams } from './buildUnsetFiltersQueryParams.js';
 export { default as generateProductsListHash } from './generateProductsListHash.js';
-export { default as getMaxDepth } from './getMaxDepth.js';
+export { default as getFacetGroupsMaxDepth } from './getFacetGroupsMaxDepth.js';
+export { default as getFacetGroupsShallowestDepth } from './getFacetGroupsShallowestDepth.js';
 export { default as getSelectedSortOption } from './getSelectedSortOption.js';
 export { default as getSlug } from './getSlug.js';
