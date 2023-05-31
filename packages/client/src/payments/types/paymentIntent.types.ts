ca88f92b08a050045da593b4ba67fb5b9a1d85a3
Ivo Mota <ivo.mota@farfetch.com>
2023-05-31 16:34:42 +0100

    feat: checkout payment capture upon shipment

--- packages/client/src/payments/types/paymentIntent.types.ts
+++ packages/client/src/payments/types/paymentIntent.types.ts
@@ -23,25 +23,54 @@ export enum PaymentIntentLineItemType {
 export type PaymentIntent = {
   id: string;
   reference: string;
+  amount: {
+    total: number;
+    items: number;
+    shipping: number;
+    paid: number;
+    remaining: number;
+  };
+  clientId: string;
   currency: string;
   dateCreated: string;
+  dueDate: string;
   status: PaymentIntentStatus;
-  totalValue: number;
-  totalValueFormattedPrice: string;
-  totalShippingFee: number;
-  formattedTotalShippingFee: string;
+  /** @deprecated use amount.total field instead */
+  totalValue?: number;
+  /** @deprecated total value formatted price will be removed soon */
+  totalValueFormattedPrice?: string;
+  /** @deprecated use amount.shipping field instead */
+  totalShippingFee?: number;
+  /** @deprecated formatted total shipping fee will be removed soon */
+  formattedTotalShippingFee?: string;
   lineItems: {
     id: string;
-    productId: string;
+    /** @deprecated use metadata.productId field instead */
+    productId?: string;
     classification: PaymentIntentLineItemClassification;
     type: PaymentIntentLineItemType;
-    unitValue: number;
-    formattedUnitValue: string;
+    /** @deprecated use lineItems.totalValue field instead */
+    unitValue?: number;
+    /** @deprecated formatted unit value will be removed soon */
+    formattedUnitValue?: string;
+    totalValue: number;
+    totalDiscount: number;
+    totalTaxes: number;
     quantity: number;
-    totalExtraTaxes: number;
-    formattedTotalExtraTaxes: string;
+    /** @deprecated use lineItems.totalTaxes field instead */
+    totalExtraTaxes?: number;
+    /** @deprecated formatted total extra taxes will be removed soon */
+    formattedTotalExtraTaxes?: string;
     description: string;
+    metadata: {
+      productId?: string;
+      productVariantId?: string;
+      productVariantDescription?: string;
+      productBrandName?: string;
+      productImageUrl?: string;
+    };
   }[];
+  locale: string;
   receiver: {
     firstName: string;
     lastName: string;
