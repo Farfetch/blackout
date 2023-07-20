5d89f1be9969436c3338825112b5158b7f444a9b
Jean Menezes <jean.menezes@farfetch.com>
2023-07-20 11:09:23 +0100

    feat(client): add social login endpoints

---
+++ packages/client/src/users/authentication/__fixtures__/deleteUserExternalLogin.fixtures.ts
@@ -0,0 +1,14 @@
+import { rest, type RestHandler } from 'msw';
+
+const path = '/api/account/v1/users/:userId/externalLogins/:externalLoginId';
+
+const fixtures = {
+  success: (): RestHandler =>
+    rest.delete(path, (_req, res, ctx) => res(ctx.status(204))),
+  failure: (): RestHandler =>
+    rest.delete(path, (_req, res, ctx) =>
+      res(ctx.status(404), ctx.json({ message: 'stub error' })),
+    ),
+};
+
+export default fixtures;
