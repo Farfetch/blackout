ec02bb8a84eeb3312d19be1fafd211c9211a6b9a
Daniel <daniel.martinez@farfetch.com>
2023-08-03 11:21:05 +0100

    feat: add shared wishlist middle for updating snapshop

---
+++ packages/redux/src/sharedWishlists/middlewares/updateSharedWishlistUponItemsChanges.ts
@@ -0,0 +1,46 @@
+import * as actionTypes from '../../wishlists/actionTypes.js';
+import {
+  getSharedWishlistId,
+  type UpdateSharedWishlistAction,
+} from '../index.js';
+import { getUser, getUserIsGuest } from '../../users/selectors.js';
+import { updateSharedWishlist } from '../actions/index.js';
+import type { AnyAction, Middleware } from 'redux';
+import type { GetOptionsArgument, StoreState } from '../../index.js';
+import type { ThunkDispatch } from 'redux-thunk';
+
+type UpdateSharedWishlistUponItemsChangesParams = {
+  // Redux action dispatch.
+  dispatch: ThunkDispatch<
+    StoreState,
+    GetOptionsArgument,
+    UpdateSharedWishlistAction
+  >;
+  // Returns the current redux state.
+  getState: () => StoreState;
+};
+
+const updateSharedWishlistUponItemsChanges: Middleware =
+  ({ dispatch, getState }: UpdateSharedWishlistUponItemsChangesParams) =>
+  next =>
+  (action: AnyAction) => {
+    if (
+      action.type === actionTypes.UPDATE_WISHLIST_ITEM_SUCCESS ||
+      action.type === actionTypes.ADD_WISHLIST_ITEM_SUCCESS ||
+      action.type === actionTypes.REMOVE_WISHLIST_ITEM_SUCCESS
+    ) {
+      const state = getState();
+      const user = getUser(state);
+      const isGuestUser = getUserIsGuest(user);
+
+      if (!isGuestUser) {
+        const sharedWishlistId = getSharedWishlistId(state);
+
+        dispatch(updateSharedWishlist(sharedWishlistId as string));
+      }
+    }
+
+    return next(action);
+  };
+
+export default updateSharedWishlistUponItemsChanges;
