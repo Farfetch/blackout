b26bed91256d0f0ae10c431fa1b37eaadb8a0316
Daniel <daniel.martinez@farfetch.com>
2023-10-18 14:30:32 +0100

    fix: avoid facets replacement when multiple listing requests

--- packages/redux/src/entities/schemas/facet.ts
+++ packages/redux/src/entities/schemas/facet.ts
@@ -1,20 +1,19 @@
-import {
-  type FacetGroup,
-  FacetType,
-  type FacetValue,
-} from '@farfetch/blackout-client';
+import { FacetType, type FacetValue } from '@farfetch/blackout-client';
 import { schema } from 'normalizr';
+import type { FacetGroupWithListingHash } from '../index.js';
 
 export const getId = (
   { value, valueUpperBound, groupsOn }: FacetValue,
-  { key, type: parentType }: FacetGroup,
+  { key, type: parentType, hash }: FacetGroupWithListingHash,
 ) =>
   `${key}_${value}${
     // Special scenario when the facet type is "sizes" or "size by category"
     parentType === FacetType.Sizes || parentType === FacetType.SizesByCategory
       ? `_${groupsOn}`
       : ''
-  }${valueUpperBound > 0 ? `_${valueUpperBound}` : ''}`;
+  }${valueUpperBound > 0 ? `_${valueUpperBound}` : ''}${
+    hash ? `_${hash}` : ''
+  }`;
 
 export default new schema.Entity(
   'facets',
@@ -24,9 +23,11 @@ export default new schema.Entity(
     processStrategy: (entity, parent) => ({
       ...entity,
       id: getId(entity, parent),
-      // Since the id is in the format "facetKey_9999999", it's only natural that the parent id is in the same format
+      // Since the id is in the format "facetKey_9999999_hash", it's only natural that the parent id is in the same format
       // (it represents an id)
-      parentId: `${parent.key}_${entity.parentId}`,
+      parentId: `${parent.key}_${entity.parentId}${
+        parent.hash ? `_${parent.hash}` : ''
+      }`,
       groupType: parent.type,
     }),
   },
