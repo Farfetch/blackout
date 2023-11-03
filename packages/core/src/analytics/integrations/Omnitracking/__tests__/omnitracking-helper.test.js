60b017a054b58e79d570c0b7fdaaed31cefb4570
Daniel Bento <danielbento92@gmail.com>
2023-11-03 15:43:36 +0000

    feat: omnitracking minor changes for `v1`
    - Add sizeItem to product_updated (size) event;
    - Fix camelCase fields on omnitracking whitelisted parameters;
    - Add warn when login or signup have method property missing;
    - Add and fix omnitracking tests and snapshots.

--- packages/core/src/analytics/integrations/Omnitracking/__tests__/omnitracking-helper.test.js
+++ packages/core/src/analytics/integrations/Omnitracking/__tests__/omnitracking-helper.test.js
@@ -5,6 +5,7 @@ import {
   getCommonCheckoutStepTrackingData,
   getDeliveryInformationDetails,
   getGenderValueFromProperties,
+  getLoginSignupRecommendedParameters,
   getPageEventFromLocation,
   getPlatformSpecificParameters,
   getRecommendationsTrackingData,
@@ -17,268 +18,300 @@ import trackTypes from '../../../types/trackTypes';
 utils.logger.warn = jest.fn();
 const mockLoggerWarn = utils.logger.warn;
 
-describe('getPageEventFromLocation', () => {
-  it('should return null when location is not provided', () => {
-    expect(getPageEventFromLocation()).toBe(null);
+describe('omnitracking-helper', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
-  it('should return null location.href is not set', () => {
-    expect(getPageEventFromLocation({})).toBe(null);
-  });
-});
+  describe('getPageEventFromLocation', () => {
+    it('should return null when location is not provided', () => {
+      expect(getPageEventFromLocation()).toBe(null);
+    });
 
-describe('getValParameterForEvent', () => {
-  it('should return a stringified object', () => {
-    expect(getValParameterForEvent()).toBe('{}');
-    expect(getValParameterForEvent({ foo: 'bar' })).toBe('{"foo":"bar"}');
-  });
-});
-
-describe('generatePaymentAttemptReferenceId', () => {
-  it('Should return a string with the generated payment attempt reference ID', () => {
-    const mockPaymentAttemptReferenceId = '12345_123456789';
-
-    expect(
-      generatePaymentAttemptReferenceId({
-        user: {
-          localId: '12345',
-        },
-        timestamp: 123456789,
-      }),
-    ).toEqual(mockPaymentAttemptReferenceId);
-  });
-});
-
-describe('getPlatformSpecificParameters', () => {
-  it('should not add parameters if the platform is mobile and track type is not screen', () => {
-    const eventData = {
-      platform: platformTypes.Mobile,
-      type: trackTypes.TRACK,
-    };
-
-    expect(getPlatformSpecificParameters(eventData)).toStrictEqual({});
-  });
-});
-
-describe('getCheckoutEventGenericProperties', () => {
-  it('should display some warn', () => {
-    const eventData = {
-      properties: {
-        orderId: '123',
-      },
-    };
-
-    getCheckoutEventGenericProperties(eventData);
-    expect(mockLoggerWarn).toHaveBeenCalledWith(
-      expect.stringContaining(
-        'property orderId should be an alphanumeric value',
-      ),
-    );
-  });
-
-  it('should get orderCode without warn and without property orderId', () => {
-    const eventData = {
-      properties: {
-        orderId: '5H5QYB',
-        checkoutOrderId: '123',
-      },
-    };
-
-    expect(getCheckoutEventGenericProperties(eventData)).toEqual({
-      orderCode: '5H5QYB',
+    it('should return null location.href is not set', () => {
+      expect(getPageEventFromLocation({})).toBe(null);
     });
   });
 
-  it('should get orderCode without warn and with property orderId', () => {
-    const eventData = {
-      properties: {
-        orderId: '5H5QYB',
-        checkoutOrderId: 123,
-      },
+  describe('getValParameterForEvent', () => {
+    it('should return a stringified object', () => {
+      expect(getValParameterForEvent()).toBe('{}');
+      expect(getValParameterForEvent({ foo: 'bar' })).toBe('{"foo":"bar"}');
+    });
+  });
+
+  describe('generatePaymentAttemptReferenceId', () => {
+    it('Should return a string with the generated payment attempt reference ID', () => {
+      const mockPaymentAttemptReferenceId = '12345_123456789';
+
+      expect(
+        generatePaymentAttemptReferenceId({
+          user: {
+            localId: '12345',
+          },
+          timestamp: 123456789,
+        }),
+      ).toEqual(mockPaymentAttemptReferenceId);
+    });
+  });
+
+  describe('getPlatformSpecificParameters', () => {
+    it('should not add parameters if the platform is mobile and track type is not screen', () => {
+      const eventData = {
+        platform: platformTypes.Mobile,
+        type: trackTypes.TRACK,
+      };
+
+      expect(getPlatformSpecificParameters(eventData)).toStrictEqual({});
+    });
+  });
+
+  describe('getCheckoutEventGenericProperties', () => {
+    it('should display some warn', () => {
+      const eventData = {
+        properties: {
+          orderId: '123',
+        },
+      };
+
+      getCheckoutEventGenericProperties(eventData);
+      expect(mockLoggerWarn).toHaveBeenCalledWith(
+        expect.stringContaining(
+          'property orderId should be an alphanumeric value',
+        ),
+      );
+    });
+
+    it('should get orderCode without warn and without property orderId', () => {
+      const eventData = {
+        properties: {
+          orderId: '5H5QYB',
+          checkoutOrderId: '123',
+        },
+      };
+
+      expect(getCheckoutEventGenericProperties(eventData)).toEqual({
+        orderCode: '5H5QYB',
+      });
+    });
+
+    it('should get orderCode without warn and with property orderId', () => {
+      const eventData = {
+        properties: {
+          orderId: '5H5QYB',
+          checkoutOrderId: 123,
+        },
+      };
+
+      expect(getCheckoutEventGenericProperties(eventData, true)).toEqual({
+        orderCode: '5H5QYB',
+        orderId: 123,
+      });
+    });
+  });
+
+  describe('getGenderValueFromProperties', () => {
+    it(`should test all ways to return ${SignupNewsletterGenderMappings[0]} value`, () => {
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: 0,
+          },
+        }),
+      ).toEqual(SignupNewsletterGenderMappings[0]);
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: '0',
+          },
+        }),
+      ).toEqual(SignupNewsletterGenderMappings[0]);
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: { id: '0' },
+          },
+        }),
+      ).toEqual(SignupNewsletterGenderMappings[0]);
+    });
+
+    it(`should test all ways to return ${SignupNewsletterGenderMappings[0]} and ${SignupNewsletterGenderMappings[1]} value`, () => {
+      const expected = `${SignupNewsletterGenderMappings[0]},${SignupNewsletterGenderMappings[1]}`;
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: [0, 1],
+          },
+        }),
+      ).toEqual(expected);
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: ['0', '1'],
+          },
+        }),
+      ).toEqual(expected);
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: [{ id: '0' }, { id: '1' }],
+          },
+        }),
+      ).toEqual(expected);
+    });
+
+    it('should return custom value', () => {
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: { name: 'W' },
+          },
+        }),
+      ).toEqual('W');
+
+      expect(
+        getGenderValueFromProperties({
+          properties: {
+            gender: [{ name: 'W' }, { name: 'M' }],
+          },
+        }),
+      ).toEqual('W,M');
+    });
+  });
+
+  describe('getDeliveryInformationDetails', () => {
+    it('should deal with empty information', () => {
+      expect(getDeliveryInformationDetails({})).toEqual(undefined);
+    });
+    it('should deal with delivery type', () => {
+      expect(
+        getDeliveryInformationDetails({
+          properties: {
+            deliveryType: 'sample',
+          },
+        }),
+      ).toEqual('{"deliveryType":"sample"}');
+    });
+
+    it('should deal with shipping tier', () => {
+      expect(
+        getDeliveryInformationDetails({
+          properties: {
+            shippingTier: 'sample',
+          },
+        }),
+      ).toEqual('{"courierType":"sample"}');
+    });
+  });
+
+  describe('getCommonCheckoutStepTrackingData', () => {
+    it('should obtain common checkout parameters', () => {
+      expect(
+        getCommonCheckoutStepTrackingData({
+          properties: {
+            step: 2,
+            deliveryType: 'sample_delivery',
+            shippingTier: 'sample_shipping',
+            interactionType: 'click',
+          },
+        }),
+      ).toEqual({
+        checkoutStep: 2,
+        deliveryInformationDetails:
+          '{"deliveryType":"sample_delivery","courierType":"sample_shipping"}',
+        interactionType: 'click',
+      });
+    });
+  });
+
+  describe('getRecommendationsTrackingData', () => {
+    const mockedRecommendationsValues = {
+      list: 'list123',
+      listId: '09a35590-bb62-4027-a630-5da04ec64fb5',
     };
 
-    expect(getCheckoutEventGenericProperties(eventData, true)).toEqual({
-      orderCode: '5H5QYB',
-      orderId: 123,
+    const mockedReturnedRecommendationsValues = {
+      moduleTitle: JSON.stringify([mockedRecommendationsValues.list]),
+      moduleId: JSON.stringify([mockedRecommendationsValues.listId]),
+    };
+
+    it('should return undefined in case its non recommendations product-details event', () => {
+      expect(
+        getRecommendationsTrackingData({
+          event: pageTypes.PRODUCT_DETAILS,
+          properties: {
+            from: 'abc',
+          },
+        }),
+      ).toBeUndefined();
     });
-  });
-});
 
-describe('getGenderValueFromProperties', () => {
-  it(`should test all ways to return ${SignupNewsletterGenderMappings[0]} value`, () => {
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: 0,
-        },
-      }),
-    ).toEqual(SignupNewsletterGenderMappings[0]);
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: '0',
-        },
-      }),
-    ).toEqual(SignupNewsletterGenderMappings[0]);
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: { id: '0' },
-        },
-      }),
-    ).toEqual(SignupNewsletterGenderMappings[0]);
-  });
-
-  it(`should test all ways to return ${SignupNewsletterGenderMappings[0]} and ${SignupNewsletterGenderMappings[1]} value`, () => {
-    const expected = `${SignupNewsletterGenderMappings[0]},${SignupNewsletterGenderMappings[1]}`;
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: [0, 1],
-        },
-      }),
-    ).toEqual(expected);
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: ['0', '1'],
-        },
-      }),
-    ).toEqual(expected);
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: [{ id: '0' }, { id: '1' }],
-        },
-      }),
-    ).toEqual(expected);
-  });
-
-  it('should return custom value', () => {
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: { name: 'W' },
-        },
-      }),
-    ).toEqual('W');
-
-    expect(
-      getGenderValueFromProperties({
-        properties: {
-          gender: [{ name: 'W' }, { name: 'M' }],
-        },
-      }),
-    ).toEqual('W,M');
-  });
-});
-
-describe('getDeliveryInformationDetails', () => {
-  it('should deal with empty information', () => {
-    expect(getDeliveryInformationDetails({})).toEqual(undefined);
-  });
-  it('should deal with delivery type', () => {
-    expect(
-      getDeliveryInformationDetails({
-        properties: {
-          deliveryType: 'sample',
-        },
-      }),
-    ).toEqual('{"deliveryType":"sample"}');
-  });
-
-  it('should deal with shipping tier', () => {
-    expect(
-      getDeliveryInformationDetails({
-        properties: {
-          shippingTier: 'sample',
-        },
-      }),
-    ).toEqual('{"courierType":"sample"}');
-  });
-});
-
-describe('getCommonCheckoutStepTrackingData', () => {
-  it('should obtain common checkout parameters', () => {
-    expect(
-      getCommonCheckoutStepTrackingData({
-        properties: {
-          step: 2,
-          deliveryType: 'sample_delivery',
-          shippingTier: 'sample_shipping',
-          interactionType: 'click',
-        },
-      }),
-    ).toEqual({
-      checkoutStep: 2,
-      deliveryInformationDetails:
-        '{"deliveryType":"sample_delivery","courierType":"sample_shipping"}',
-      interactionType: 'click',
+    it('should return empty object if the event is sent without the recommendations parameters filled', () => {
+      expect(
+        getRecommendationsTrackingData({
+          event: 'abc',
+          properties: {
+            from: fromParameterTypes.RECOMMENDATIONS,
+          },
+        }),
+      ).toEqual({});
     });
-  });
-});
 
-describe('getRecommendationsTrackingData', () => {
-  const mockedRecommendationsValues = {
-    list: 'list123',
-    listId: '09a35590-bb62-4027-a630-5da04ec64fb5',
-  };
+    it('should return the json stringified values correctly', () => {
+      expect(
+        getRecommendationsTrackingData({
+          event: 'abc',
+          properties: {
+            from: fromParameterTypes.RECOMMENDATIONS,
+            ...mockedRecommendationsValues,
+          },
+        }),
+      ).toEqual(mockedReturnedRecommendationsValues);
+    });
 
-  const mockedReturnedRecommendationsValues = {
-    moduleTitle: JSON.stringify([mockedRecommendationsValues.list]),
-    moduleId: JSON.stringify([mockedRecommendationsValues.listId]),
-  };
+    it('should return just one stringified parameter in case its sent only one parameter', () => {
+      expect(
+        getRecommendationsTrackingData({
+          event: 'abc',
+          properties: {
+            from: 'abc',
+            list: mockedRecommendationsValues.list,
+          },
+        }),
+      ).toEqual({
+        moduleTitle: mockedReturnedRecommendationsValues.moduleTitle,
+      });
+    });
 
-  it('should return undefined in case its non recommendations product-details event', () => {
-    expect(
-      getRecommendationsTrackingData({
-        event: pageTypes.PRODUCT_DETAILS,
-        properties: {
-          from: 'abc',
-        },
-      }),
-    ).toBeUndefined();
-  });
+    describe('getLoginSignupRecommendedParameters', () => {
+      it('should log a warning when recommended properties are missing and should not return any properties.', () => {
+        expect(
+          getLoginSignupRecommendedParameters({
+            event: 'abc',
+            properties: {},
+          }),
+        ).toEqual({});
 
-  it('should return empty object if the event is sent without the recommendations parameters filled', () => {
-    expect(
-      getRecommendationsTrackingData({
-        event: 'abc',
-        properties: {
-          from: fromParameterTypes.RECOMMENDATIONS,
-        },
-      }),
-    ).toEqual({});
-  });
+        expect(mockLoggerWarn).toHaveBeenCalled();
+      });
 
-  it('should return the json stringified values correctly', () => {
-    expect(
-      getRecommendationsTrackingData({
-        event: 'abc',
-        properties: {
-          from: fromParameterTypes.RECOMMENDATIONS,
-          ...mockedRecommendationsValues,
-        },
-      }),
-    ).toEqual(mockedReturnedRecommendationsValues);
-  });
+      it('should not log a warning when all recommended properties are filled and correctly mapped to Omnitracking fields.', () => {
+        expect(
+          getLoginSignupRecommendedParameters({
+            event: 'abc',
+            properties: {
+              method: 'test',
+            },
+          }),
+        ).toEqual({ loginType: 'test' });
 
-  it('should return just one stringified parameter in case its sent only one parameter', () => {
-    expect(
-      getRecommendationsTrackingData({
-        event: 'abc',
-        properties: {
-          from: 'abc',
-          list: mockedRecommendationsValues.list,
-        },
-      }),
-    ).toEqual({
-      moduleTitle: mockedReturnedRecommendationsValues.moduleTitle,
+        expect(mockLoggerWarn).not.toHaveBeenCalled();
+      });
     });
   });
 });
