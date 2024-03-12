5cace1a4806988d58460df807706823cb4b0fa3b
Bruno Oliveira <bruno.a.oliveira@farfetch.com>
2024-03-12 18:49:16 +0000

    fix: fix ssr
    This fixes the broken ssr introduced by the
    empty catch block in GoogleConsentMode from blackout-react/analytics.
    Also fixes some jsdoc errors and changes the version calculation
    code in analytics from blackout-react/analytics.

--- packages/react/src/analytics/constants.js
+++ packages/react/src/analytics/constants.js
@@ -1,8 +1,4 @@
-// We use a require here to avoid typescript complaining of `package.json` is not
-// under rootDir that we would get if we used an import. Typescript apparently ignores
-// requires.
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { name, version } = require('../../package.json');
+import { name, version } from '../../package.json';
 
 export const PACKAGE_VERSION = version;
 export const PACKAGE_NAME = name;
